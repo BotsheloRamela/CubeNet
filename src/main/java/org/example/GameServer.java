033554a29309b61a1c72079a9821f50908445fb7
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-21 14:16:21 +0200

    feat(core): Created methods to read & write player positions to clients

--- src/main/java/org/example/GameServer.java
+++ src/main/java/org/example/GameServer.java
@@ -16,4 +16,59 @@ public class GameServer {
             System.out.println("IOException from GameServer");
         }
     }
+
+    public void acceptConnections() {
+        try {
+            System.out.println("Waiting for connections...");
+            while (numPlayers < maxPlayers) {
+                Socket socket = serverSocket.accept();
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+                playerSockets[numPlayers] = socket;
+                numPlayers++;
+
+                out.writeInt(numPlayers);
+                System.out.println("Player #" + numPlayers + " has connected.");
+
+                if (numPlayers == 2) {
+                    DataInputStream in1 = new DataInputStream(playerSockets[0].getInputStream());
+                    DataOutputStream out1 = new DataOutputStream(playerSockets[0].getOutputStream());
+                    DataInputStream in2 = new DataInputStream(playerSockets[1].getInputStream());
+                    DataOutputStream out2 = new DataOutputStream(playerSockets[1].getOutputStream());
+                }
+            }
+            System.out.println("No longer accepting connections");
+        } catch (IOException e) {
+            System.out.println("IOException from acceptConnection() in GameServer");
+        }
+    }
+
+    private void readFromClient(DataInputStream in, int playerID) {
+        try {
+            while (true) {
+                playerPositions[playerID][0] = in.readDouble();
+                playerPositions[playerID][1] = in.readDouble();
+            }
+        } catch (IOException e) {
+            System.out.println("IOException from RFC run() in GameServer");
+        }
+    }
+
+    private void writeToClient(DataOutputStream out, int playerID) {
+        try {
+            out.writeUTF("We now have 2 players. GO!");
+            while (true) {
+                int otherPlayerID = (playerID + 1) % 2;
+                out.writeDouble(playerPositions[otherPlayerID][0]);
+                out.writeDouble(playerPositions[otherPlayerID][1]);
+                out.flush(); // Ensures that the data is sent immediately.
+                Thread.sleep(25); // Prevents the loop from running too quickly and using up too much CPU time.
+            }
+        } catch (IOException e) {
+            System.out.println("IOException from WTC run() in GameServer");
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException from WTC run() in GameServer");
+        }
+    }
 }
