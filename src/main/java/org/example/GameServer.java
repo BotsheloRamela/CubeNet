ee051b4454c0c53d5554850049f5d61ae177079e
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-21 14:14:29 +0200

    feat(core): Created GameServer constructor to create a socket & a method to accpet connections

--- src/main/java/org/example/GameServer.java
+++ src/main/java/org/example/GameServer.java
@@ -1,4 +1,19 @@
 package org.example;
+import java.io.*;
+import java.net.*;
 
 public class GameServer {
+    private ServerSocket serverSocket;
+    private int numPlayers = 0, maxPlayers = 2;
+    private Socket[] playerSockets = new Socket[2];
+    private double[][] playerPositions = {{100, 400}, {490, 400}};
+
+    public GameServer() {
+        System.out.println("===== GAME SERVER =====");
+        try {
+            serverSocket = new ServerSocket(4545);
+        } catch (IOException e) {
+            System.out.println("IOException from GameServer");
+        }
+    }
 }
