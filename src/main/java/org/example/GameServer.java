1b81f280f2599c12d9ac2d398956890a8c91bdb4
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-21 14:19:47 +0200

    docs(core): Added docs for the Game Server logic

--- src/main/java/org/example/GameServer.java
+++ src/main/java/org/example/GameServer.java
@@ -1,13 +1,25 @@
+/**
+
+ The GameServer class represents a server that coordinates communication between two clients playing a game.
+ The server listens for incoming connections, accepts two clients, and starts two threads to handle communication
+ between the two clients. The server constantly updates the positions of the players and sends the updated positions
+ to the clients.
+ */
+
 package org.example;
 import java.io.*;
 import java.net.*;
 
 public class GameServer {
-    private ServerSocket serverSocket;
+    private ServerSocket serverSocket; // The ServerSocket object used to listen for incoming connections.
+    // The number of players currently connected to the server. | The maximum number of players that can connect to the server.
     private int numPlayers = 0, maxPlayers = 2;
-    private Socket[] playerSockets = new Socket[2];
-    private double[][] playerPositions = {{100, 400}, {490, 400}};
+    private Socket[] playerSockets = new Socket[2]; // An array of sockets representing the player connections.
+    private double[][] playerPositions = {{100, 400}, {490, 400}}; // An array of arrays representing the positions of each player.
 
+    /**
+     * Constructs a new GameServer object and creates a ServerSocket to listen for incoming connections.
+     */
     public GameServer() {
         System.out.println("===== GAME SERVER =====");
         try {
@@ -17,6 +29,10 @@ public class GameServer {
         }
     }
 
+    /**
+     * Listens for incoming connections and accepts two players.
+     * Starts two threads to handle communication between the players.
+     */
     public void acceptConnections() {
         try {
             System.out.println("Waiting for connections...");
@@ -44,6 +60,13 @@ public class GameServer {
         }
     }
 
+    /**
+     * Reads the position of a player from the given DataInputStream and updates the corresponding position
+     * in the playerPositions array.
+     *
+     * @param in The DataInputStream to read from.
+     * @param playerID The ID of the player whose position is being read.
+     */
     private void readFromClient(DataInputStream in, int playerID) {
         try {
             while (true) {
@@ -55,6 +78,13 @@ public class GameServer {
         }
     }
 
+    /**
+     * Writes data to a client through a DataOutputStream object.
+     * It sends the position of the other player in the game to the client in a continuous loop.
+     *
+     * @param out The output stream to write to the client.
+     * @param  playerID The ID of the player whose position is not being sent.
+     */
     private void writeToClient(DataOutputStream out, int playerID) {
         try {
             out.writeUTF("We now have 2 players. GO!");
