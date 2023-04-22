ff70e1329c558e7ef2410cd524100fcc37c2f7bc
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-22 11:23:06 +0200

    feat(core)!: Added PlayerSprite constructor & drawSprite method

--- src/main/java/org/example/PlayerSprite.java
+++ src/main/java/org/example/PlayerSprite.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
 public class PlayerSprite {
+
+    private double x;
+    private double y;
+    private final double size;
+    private final Color color;
+
+    public PlayerSprite(double x, double y, double size, Color color) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+    }
+
+    public void drawSprite(Graphics2D graphics2D) {
+        Rectangle2D.Double square = new Rectangle2D.Double(x, y, size, size);
+        graphics2D.setColor(color);
+        graphics2D.fill(square);
+    }
 }
