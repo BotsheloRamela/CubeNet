cc7cd26a28283b8e96ce058a8e46df20de37b4a6
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-22 14:13:23 +0200

    docs(core): Added documentation for ClientHandler

--- src/main/java/org/example/client/ClientHandler.java
+++ src/main/java/org/example/client/ClientHandler.java
@@ -1,3 +1,9 @@
+/**
+ * A class representing a client handler for the game server.
+ * Each instance of this class represents a client connected to the server.
+ * It manages the communication with the client, exchanging player positions with the other client.
+ */
+
 package org.example.client;
 
 import java.io.DataInputStream;
@@ -14,6 +20,13 @@ public class ClientHandler extends Thread {
     private DataInputStream in;
     private DataOutputStream out;
 
+    /**
+     * Creates a new instance of the ClientHandler class.
+     * @param playerId the ID of the player being handled by this client handler
+     * @param socket the socket associated with the client
+     * @param in the input stream associated with the socket
+     * @param out the output stream associated with the socket
+     */
     public ClientHandler(int playerId, Socket socket, DataInputStream in, DataOutputStream out) {
         this.playerId = playerId;
         this.socket = socket;
@@ -21,19 +34,27 @@ public class ClientHandler extends Thread {
         this.out = out;
     }
 
+    /**
+     * Runs the client handler thread.
+     * Sends the welcome message to the client and waits until the maximum number of players is reached.
+     * Then, continuously exchanges player positions with the other client.
+     */
     @Override
     public void run() {
         try {
+            // Send welcome message and initial player position to client
             out.writeInt(numPlayers);
             out.writeUTF("Welcome, player #" + playerId + "!");
             out.writeDouble(playerPositions[playerId-1][0]);
             out.writeDouble(playerPositions[playerId-1][1]);
             out.flush();
 
+            // Wait until the maximum number of players is reached
             while (numPlayers < maxPlayers) {
                 Thread.sleep(1000);
             }
 
+            // Continuously exchange player positions with the other client
             while (true) {
                 int otherPlayerId = (playerId == 1) ? 2 : 1;
                 double otherPlayerX = playerPositions[otherPlayerId-1][0];
@@ -52,6 +73,7 @@ public class ClientHandler extends Thread {
         } catch (InterruptedException e) {
             System.out.println("InterruptedException from ClientHandler run()");
         } finally {
+            // Close the socket
             try {
                 socket.close();
             } catch (IOException e) {
