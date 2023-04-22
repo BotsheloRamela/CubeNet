aea46b7a48e5bd7d95535837dac8fbfb4bf9213c
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-22 14:06:42 +0200

    feat(core): Extended ClientHandler to use Thread + Added comms logic

--- src/main/java/org/example/client/ClientHandler.java
+++ src/main/java/org/example/client/ClientHandler.java
@@ -2,9 +2,10 @@ package org.example.client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
-public class ClientHandler {
+public class ClientHandler extends Thread {
 
     private int numPlayers = 0, maxPlayers = 2;
     private double[][] playerPositions = {{100, 400}, {490, 400}};
@@ -18,5 +19,44 @@ public class ClientHandler {
         this.socket = socket;
         this.in = in;
         this.out = out;
+    }
+
+    @Override
+    public void run() {
+        try {
+            out.writeInt(numPlayers);
+            out.writeUTF("Welcome, player #" + playerId + "!");
+            out.writeDouble(playerPositions[playerId-1][0]);
+            out.writeDouble(playerPositions[playerId-1][1]);
+            out.flush();
+
+            while (numPlayers < maxPlayers) {
+                Thread.sleep(1000);
+            }
+
+            while (true) {
+                int otherPlayerId = (playerId == 1) ? 2 : 1;
+                double otherPlayerX = playerPositions[otherPlayerId-1][0];
+                double otherPlayerY = playerPositions[otherPlayerId-1][1];
+                out.writeDouble(otherPlayerX);
+                out.writeDouble(otherPlayerY);
+                out.flush();
+
+                double myPlayerX = in.readDouble();
+                double myPlayerY = in.readDouble();
+                playerPositions[playerId-1][0] = myPlayerX;
+                playerPositions[playerId-1][1] = myPlayerY;
+            }
+        } catch (IOException e) {
+            System.out.println("IOException from ClientHandler run()");
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException from ClientHandler run()");
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                System.out.println("IOException from ClientHandler finally block");
+            }
+        }
     }
 }
